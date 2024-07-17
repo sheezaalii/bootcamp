d14bf399246c687cb878ba7fc4b4fcb8341f078f
Sheeza Ali <106429982+sheezaalii@users.noreply.github.com>
2024-07-17 19:34:10 +0500

    submit

---
+++ Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter size of array: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[50];
+
+            Console.WriteLine("elements before insertion: ");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("Array: ");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(array[i]+ " ");
+            }
+
+            Console.Write("\nEnter position for insertion of element: ");
+            int pos = int.Parse(Console.ReadLine());
+            Console.Write("Enter value to be inserted : ");
+            int value = int.Parse(Console.ReadLine());
+
+            if (pos < 0 || pos > size + 1)
+            {
+                Console.WriteLine("invalid position");
+            }
+            else
+            {
+                for (int i = size - 1; i >= pos - 1; i--)
+                {
+                    array[size + 1] = array[size];
+                }
+                array[pos-1] = value;
+                size++;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+        }
+    }
+}
---
+++ quiz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyApp
+{
+            //Have the function ArrayAddition1(arr) take the array of numbers stored in arr 
+            //and return the string true if any combination of numbers in the array[excluding the Largest number) 
+            //can be added up to equal the largest number in the array, otherwise retum the string false.
+            //For example if arr contains[4, 6, 23, 10, 1, 3] then output should return true because 4 + 6 + 10 + 3 = 23.
+            //The array will not be empty, will not contain all the same elements, and may contains negative numbers.
+    internal class  Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool ArrayAddition1(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+            }
+            return true;
+        }
+
+        public static int Largest(int[] arr) 
+        {
+            int largest = 0;
+            for (int i = 0; i < arr.Length; i++) 
+            { 
+                if(arr[i] > largest)
+                {
+                    largest = arr[i];
+                }
+            }
+            return largest;
+        }
+
+       
+    }
+}
